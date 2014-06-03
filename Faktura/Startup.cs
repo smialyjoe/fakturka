f60b88893a38fbd10531e4326a4e95aa4d8dce3a
smialyjoe <smialyjoe@gmail.com>
2014-06-03 21:35:15 +0200

    wrzucenie solucji do repo

---
+++ Faktura/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Faktura.Startup))]
+namespace Faktura
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
